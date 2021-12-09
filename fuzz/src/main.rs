6fc9311005dbf9ee4d0e8e2231269b55add9c92a
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2021-12-10 00:32:38 +0100

    wip

--- fuzz/src/main.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    afl::fuzz!(|data: &[u8]| {
-        if let Ok(s) = std::str::from_utf8(data) {
-            for token in html5gum::Tokenizer::new(s).infallible() {
-            }
-        }
-    });
-}
