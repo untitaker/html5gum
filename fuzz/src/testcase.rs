aa91b325c8a89649361a54bc43f5fb3d2f4cda4a
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2021-12-15 23:01:16 +0100

    avoid using system afl

--- fuzz/src/testcase.rs
+++ fuzz/src/testcase.rs
@@ -20,11 +20,15 @@ pub fn run(s: &str) {
     if env::var("FUZZ_OLD_HTML5GUM").unwrap() == "1" {
         let reference_tokenizer = html5gum_old::Tokenizer::new(s).infallible();
         let testing_tokenizer = html5gum::Tokenizer::new(s).infallible();
+        let mut testing_tokens = Vec::new();
+        let mut reference_tokens = Vec::new();
         for (a, b) in testing_tokenizer.zip(reference_tokenizer) {
             // Check equality of two different versions of Token by... stringifying them
-            assert_eq!(format!("{:?}", a), format!("{:?}", b));
+            testing_tokens.push(format!("{:?}", a));
+            reference_tokens.push(format!("{:?}", b));
         }
 
+        assert_eq!(testing_tokens, reference_tokens);
         did_anything = true;
     }
 
