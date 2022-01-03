fd9d51293f0dc8d2a4e772a2b186b8bdad21e345
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2022-01-03 20:48:59 +0100

    add benchmarks

---
+++ benches/data_state.rs
@@ -0,0 +1,15 @@
+use html5gum::Tokenizer;
+use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId};
+
+fn data_state(c: &mut Criterion) {
+    for i in [100, 1000, 10000] {
+        let s: String = (0..i).map(|_| 'a').collect();
+        c.bench_with_input(
+            BenchmarkId::new("aaa", i), &s,
+            |b, s| b.iter(|| for _ in Tokenizer::new(s).infallible() {})
+        );
+    }
+}
+
+criterion_group!(benches, data_state);
+criterion_main!(benches);
