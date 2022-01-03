24ad67159205f4a21c3729f27a5e7d0bb7b0fb14
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2022-01-03 21:11:55 +0100

    benchmark updates

--- benches/data_state.rs
+++ benches/data_state.rs
@@ -2,7 +2,7 @@ use html5gum::Tokenizer;
 use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId};
 
 fn data_state(c: &mut Criterion) {
-    for i in [100, 1000, 10000] {
+    for i in [100, 1000, 10000, 1000000] {
         let s: String = (0..i).map(|_| 'a').collect();
         c.bench_with_input(
             BenchmarkId::new("aaa", i), &s,
