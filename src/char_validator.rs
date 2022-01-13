ea9fb852bd808dbb4f47e974bfce8d2a3ae2dce2
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2022-01-13 19:42:25 +0100

    fix new test

--- src/char_validator.rs
+++ src/char_validator.rs
@@ -21,11 +21,14 @@ impl CharValidator {
     #[inline]
     pub fn validate_byte<E: Emitter>(&mut self, emitter: &mut E, next_byte: u8) {
         if next_byte < 128 {
+            // start of character (ascii)
             self.last_4_bytes = 0;
             self.flush_character_error(emitter);
             self.validate_last_4_bytes(emitter, next_byte as u32);
         } else if next_byte >= 192 {
+            // start of character (non-ascii)
             self.last_4_bytes = next_byte as u32;
+            self.flush_character_error(emitter);
         } else {
             self.last_4_bytes <<= 8;
             self.last_4_bytes |= next_byte as u32;
