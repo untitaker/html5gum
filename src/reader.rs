a93b3335a02631b83228f78173fc2c7fc139d780
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2023-07-31 17:33:44 +0200

    try and remove jetscii

--- src/reader.rs
+++ src/reader.rs
@@ -426,15 +426,5 @@ impl<'a> Readable<'a> for File {
 
 #[inline]
 fn fast_find(needle: &[u8], haystack: &[u8]) -> Option<usize> {
-    #[cfg(feature = "jetscii")]
-    {
-        debug_assert!(needle.len() <= 16);
-        let mut needle_arr = [0; 16];
-        needle_arr[..needle.len()].copy_from_slice(needle);
-        #[allow(clippy::cast_possible_truncation, clippy::cast_possible_wrap)]
-        jetscii::Bytes::new(needle_arr, needle.len() as i32, |b| needle.contains(&b)).find(haystack)
-    }
-
-    #[cfg(not(feature = "jetscii"))]
     haystack.iter().position(|b| needle.contains(b))
 }
