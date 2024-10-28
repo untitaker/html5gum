ff5e9673f57671c2e6efd0b7015249a3e55abd9a
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2024-10-28 20:36:05 +0100

    fix warning

--- tests/html5lib_tree_builder.rs
+++ tests/html5lib_tree_builder.rs
@@ -175,9 +175,7 @@ fn build_test(testcase: Testcase, fname: &str, i: usize, scripting: bool) -> Tri
                 tokenizer.set_state(state);
             }
 
-            for result in tokenizer {
-                result.unwrap();
-            }
+            tokenizer.finish().unwrap();
 
             let rcdom = tree_builder.sink;
             let mut actual = String::new();
