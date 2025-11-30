744adc607ad21ca4a579b40423e0363a36ce9006
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2025-11-30 23:11:26 +0100

    Cache envvars in fuzzing target

--- fuzz/src/testcase/mod.rs
+++ fuzz/src/testcase/mod.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::sync::LazyLock;
 
 mod html5ever;
 mod lolhtml;
@@ -6,15 +7,26 @@ mod old_html5gum;
 mod span_invariants;
 mod swc;
 
+// Cache environment variable lookups to avoid repeated syscalls
+static FUZZ_BASIC: LazyLock<bool> = LazyLock::new(|| env::var("FUZZ_BASIC").unwrap() == "1");
+static FUZZ_OLD_HTML5GUM: LazyLock<bool> =
+    LazyLock::new(|| env::var("FUZZ_OLD_HTML5GUM").unwrap() == "1");
+static FUZZ_HTML5EVER: LazyLock<bool> =
+    LazyLock::new(|| env::var("FUZZ_HTML5EVER").unwrap() == "1");
+static FUZZ_LOLHTML: LazyLock<bool> = LazyLock::new(|| env::var("FUZZ_LOLHTML").unwrap() == "1");
+static FUZZ_SWC: LazyLock<bool> = LazyLock::new(|| env::var("FUZZ_SWC").unwrap() == "1");
+static FUZZ_SPAN_INVARIANTS: LazyLock<bool> =
+    LazyLock::new(|| env::var("FUZZ_SPAN_INVARIANTS").unwrap() == "1");
+
 pub fn run(s: &[u8]) {
-    let mut did_anything = env::var("FUZZ_BASIC").unwrap() == "1";
+    let mut did_anything = *FUZZ_BASIC;
 
     // unconditionally run tokenizer against raw bytes, it should never crash. we rely on running
     // in debug mode such that this is not just simply optimized away
     let testing_tokenizer = html5gum::Tokenizer::new(s);
     for Ok(_) in testing_tokenizer {}
 
-    if env::var("FUZZ_OLD_HTML5GUM").unwrap() == "1" {
+    if *FUZZ_OLD_HTML5GUM {
         if let Ok(data) = std::str::from_utf8(s) {
             old_html5gum::run_old_html5gum(data);
         }
@@ -22,7 +34,7 @@ pub fn run(s: &[u8]) {
         did_anything = true;
     }
 
-    if env::var("FUZZ_HTML5EVER").unwrap() == "1" {
+    if *FUZZ_HTML5EVER {
         if let Ok(data) = std::str::from_utf8(s) {
             html5ever::run_html5ever(data);
         }
@@ -30,19 +42,19 @@ pub fn run(s: &[u8]) {
         did_anything = true;
     }
 
-    if env::var("FUZZ_LOLHTML").unwrap() == "1" {
+    if *FUZZ_LOLHTML {
         lolhtml::run_lolhtml(s);
         did_anything = true;
     }
 
-    if env::var("FUZZ_SWC").unwrap() == "1" {
+    if *FUZZ_SWC {
         if let Ok(data) = std::str::from_utf8(s) {
             swc::run_swc(data);
         }
         did_anything = true;
     }
 
-    if env::var("FUZZ_SPAN_INVARIANTS").unwrap() == "1" {
+    if *FUZZ_SPAN_INVARIANTS {
         span_invariants::validate_span_invariants(s);
         did_anything = true;
     }
