6bcdeacb55b83c2ecc9e50bcb932c2d8e342519a
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2025-12-01 11:33:54 +0100

    wip

--- src/machine.rs
+++ src/machine.rs
@@ -272,7 +272,9 @@ pub(crate) mod states {
                     reconsume_in!(slf, Some(x), RcDataEndTagName)
                 }
                 c => {
+                    slf.emitter.move_position(-1);
                     slf.emitter.emit_string(b"</");
+                    slf.emitter.move_position(1);
                     reconsume_in!(slf, c, RcData)
                 }
             }
